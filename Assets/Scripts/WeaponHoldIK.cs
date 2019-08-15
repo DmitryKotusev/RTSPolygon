91cd94b33ec387c4b4f591c380358c742968cc95
DmitryKotusev <dssa-1966@yandex.ru>
2019-08-15 22:58:38 +0300

    Init commit.

---
+++ Assets/Scripts/WeaponHoldIK.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHoldIK : MonoBehaviour
+{
+    [SerializeField]
+    WeaponIKInfo currentWeapon;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public WeaponIKInfo CurrentWeapon
+    {
+        get
+        {
+            return currentWeapon;
+        }
+
+        set
+        {
+            currentWeapon = value;
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, CurrentWeapon.LeftHandTransform.position);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, CurrentWeapon.LeftHandTransform.rotation);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+    }
+}
